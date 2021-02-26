71596b275515a9c80bc467ba9390c16f76f97fcd
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-02-25 18:48:09 -0800

    Adding imports to app.js and components

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import Header from "./components/Header";
+import Table from "./components/Table";
+import TableRow from "./components/TableRow";
+import API from "./utils/API";
 
 class App extends Component {
 
---
+++ employee-directory/src/components/Header/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+function Header(props) {
+
+}
+
+export default Header;
