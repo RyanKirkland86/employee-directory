95369172a90dafbf83b2e253b9b8b25595f62233
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-02-25 21:03:19 -0800

    Code for Header component, installed axios

--- employee-directory/src/components/Header/index.js
+++ employee-directory/src/components/Header/index.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./style.css";
 
 function Header(props) {
-
+    return (
+        <div className="jumbotron jumbotron-fluid" id="header">
+            <div className="container">
+                <h1 className="display-4 align-items-center">Employee Directory</h1>
+                    <p className="lead align-items-center">
+                        Click on carrots to filter by heading or use the search box to narrow your results.
+                    </p>
+            </div>
+        </div>
+    )
 }
 
 export default Header;
